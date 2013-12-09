3c330189543f89aa328ae0b0b56f40eba9445d0b
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2013-12-10 01:00:12 +0530

    Added 2 tabs to the landing page using Fragments.

--- src/com/example/gittrial/MainActivity.java
+++ src/com/example/gittrial/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.gittrial;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
+	private FragmentTabHost tabHost;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.landing);
+		tabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+		tabHost.setup(this,getSupportFragmentManager(),R.id.realtabcontent);
+		
+		tabHost.addTab(tabHost.newTabSpec("News").setIndicator("News"),NewsTabFragment.class, null);
+		tabHost.addTab(tabHost.newTabSpec("Events").setIndicator("Events"),EventsTabFragment.class, null);
+			
 	}
 
 	@Override
